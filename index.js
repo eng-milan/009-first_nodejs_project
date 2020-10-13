a4c2887c417cc7a65157dc57cd44cd9516a1892e
tohid_d.milan@yahoo.com <tohid_d.milan@yahoo.com>
2020-10-13 07:24:14 -0600

    adds 2 npm and index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+const supervillains = require("supervillains");
+const superheroes = require("superheroes");
+const supervillainName = supervillains.random();
+const superheroName = superheroes.random();
+console.log("This is my supervillain name: " + supervillainName);
+console.log("This is my superhero name: " + superheroName);
